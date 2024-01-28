d0beae9703e84d5885168334694e3e148ca7fbf2
Arthip Srain <117334939+arthipsama@users.noreply.github.com>
2024-01-28 21:57:22 +0700

    logout pop-up moch jahhh

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { ColorService } from '../service/color.service';
 import { NavigationEnd, Router } from '@angular/router';
+import { AlertServiceService } from '../service/alert-service.service';
 
 @Component({
   selector: 'app-admin',
@@ -12,6 +13,7 @@ export class AdminComponent implements OnInit {
   constructor(private el:ElementRef , 
     private colorService: ColorService,
     private router: Router,
+    private alertService: AlertServiceService,
     ){
 
     this.router.events.subscribe(event => {
@@ -29,6 +31,17 @@ export class AdminComponent implements OnInit {
 
   isActive(route: string): boolean {
     return this.router.isActive(route, true);
+  }
+  
+  logout() {
+    this.alertService.onConfirmRoute('ต้องการออกจากระบบ', '/mainpage')
+      .then((confirmed) => {
+        if (confirmed) {
+          this.alertService.onSuccess('ออกจากระบบสำเร็จ')
+        } else {
+          // กรณีผู้ใช้กด "Cancel" ไม่ต้องทำอะไร
+        }
+      });
   }
   
 
