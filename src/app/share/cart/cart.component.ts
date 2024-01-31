b65bb11a6a3f66a610112b17945e07bd46c0dbcd
nattawut <wiencompop1168@gmail.com>
2024-01-31 18:46:38 +0700

    cart  but not have delete

--- src/app/share/cart/cart.component.ts
+++ src/app/share/cart/cart.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { productData } from 'src/app/models/product.model';
+import { userData } from 'src/app/models/user.models';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-cart',
@@ -8,16 +11,39 @@ import { Router } from '@angular/router';
 })
 export class CartComponent {
   haveItems: boolean = true;
+  userData!: userData;
+  userProducts: any;
+  rowTotal:any;
+  totalprice:any;
 
-  constructor(private router: Router){
+  constructor(private router: Router,
+              private service: ProductService){
 
   }
 
   ngOnInit(){
-    
+    let storedUserData = localStorage.getItem('userData');
+    if (storedUserData) {
+        this.userData = JSON.parse(storedUserData);
+    }
+    this.service.cart(this.userData.userid).subscribe(x=>{
+      this.userProducts = x;
+      if(this.userProducts){
+        this.rowTotal = this.userProducts.map((product:productData) =>{
+          return product.price * product.quantity
+        }) 
+      }
+      if(this.userProducts){
+        this.totalprice = this.rowTotal.reduce((accumulator:any, currentValue:any) => accumulator + currentValue, 0);
+      }
+    })
   }
 
   gotomainpage(){
     this.router.navigate(['/mainpage']);
+  }
+
+  payProduct(){
+    
   }
 }
