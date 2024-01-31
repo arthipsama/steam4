e715ff766c01122ac2d43efc093342312841998e
Arthip Srain <117334939+arthipsama@users.noreply.github.com>
2024-01-31 14:32:25 +0700

    add row user fix col

--- src/app/admin/account/account.component.ts
+++ src/app/admin/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AccountComponent {
 
+
+  users = [
+    {
+      userId: '#5033',
+      // imagePath: '../assets/role/user.png',
+      username: 'Jassa1',
+      email: 'jassa@jassa.orgjassa@jassajassajassajassajassaasdasdasdasdasd',
+      firstName: 'Arthip',
+      phone: '0987129295',
+      role: 'USER'
+    },
+    {
+      userId: '#5034',
+      // imagePath: '../assets/role/admin.png', // แก้ที่นี้ให้ตรงกับที่คุณต้องการ
+      username: 'Jassa2',
+      email: 'jassa@jassa.orgjassa@jassajassajassajassajassaasdasdasdasdasd',
+      firstName: 'Arthip',
+      phone: '0987129295',
+      role: 'ADMIN'
+    },
+    {
+      userId: '#5035',
+      // imagePath: '../assets/role/user.png', // แก้ที่นี้ให้ตรงกับที่คุณต้องการ
+      username: 'Jassa3',
+      email: 'jassa@jassa.orgjassa@jassajassajassajassajassaasdasdasdasdasd',
+      firstName: 'Arthip',
+      phone: '0987129295',
+      role: 'USER'
+    },
+    // เพิ่มข้อมูลอื่น ๆ ตามต้องการ
+  ];
+
+  getImagePath(role: string): string {
+    return role === 'ADMIN' ? '../assets/role/admin.png' : '../assets/role/user.png';
+  }
+  
+  
 }
